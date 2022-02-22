e26d24800fed101d9e5fd146a8f41627f3726eb3
reza007bm <mreza.sec698@gmail.com>
2022-02-22 11:23:24 +0330

    Updates

---
+++ precache-manifest.526ae01e16204ff603f960208f5f3a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce58e9c959dee6c6c219e211fc231a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0b07ddb8e6d124d7737",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "426d18ecd0ac4d1dd9ac",
+    "url": "/static/css/main.0e2c77e8.chunk.css"
+  },
+  {
+    "revision": "c0b07ddb8e6d124d7737",
+    "url": "/static/js/2.4f65913a.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.4f65913a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "426d18ecd0ac4d1dd9ac",
+    "url": "/static/js/main.8289e9b0.chunk.js"
+  },
+  {
+    "revision": "b47c9a5d6a8c33c55c6d",
+    "url": "/static/js/runtime-main.01869917.js"
+  }
+]);
